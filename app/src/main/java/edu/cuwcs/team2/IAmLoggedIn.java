33721f0b3ead6ecbc389a69ab00ce84ddc95e386
Collin2245 <collin2245@gmail.com>
2019-04-13 18:38:39 -0500

    Collin K's commit.

--- app/src/main/java/edu/cuwcs/team2/IAmLoggedIn.java
+++ app/src/main/java/edu/cuwcs/team2/IAmLoggedIn.java
@@ -1,7 +1,9 @@
 package edu.cuwcs.team2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class IAmLoggedIn extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class IAmLoggedIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iam_logged_in);
+
+
+
+
+
     }
+
+    public void toFutureMoneyCalculatorButtonPressed(View v)
+    {
+        Intent i = new Intent(this, CalcActivity.class);
+        this.startActivity(i);
+    }
+
+    public void toUpdateProfileButtonPressed(View v)
+    {
+        Intent i = new Intent(this, updateActivity.class);
+        this.startActivity(i);
+    }
+
 }
